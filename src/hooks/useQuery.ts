179c101cfa255d32e86956f98c27ad5049f4055c
Basile Bruneau <basile@bruneau.email>
2022-11-08 14:27:19 +0100

    feat(useQuery): add fetchMore function

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -7,12 +7,16 @@ interface Options {
   skip?: boolean;
 }
 
+type MergeFn<T> = (prevData: T, newData: T) => T;
+
 export const useQuery = <RestType>(path: string, options?: Options) => {
   const { query, unsubscribe } = useRestCache();
   const [, setIncrement] = useState(0); // Only way to force a re-render
   const [data, setData] = useState<RestType | undefined>(undefined);
   const [error, setError] = useState<Error | undefined>(undefined);
   const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [abortControllers] = useState<Set<AbortController>>(new Set());
   const rerender = useCallback(
     () => setIncrement((i) => i + 1),
     [setIncrement]
@@ -22,6 +26,7 @@ export const useQuery = <RestType>(path: string, options?: Options) => {
     setLoading(true);
 
     const abortController = new AbortController();
+    abortControllers.add(abortController);
     const signal = abortController.signal;
 
     query<RestType>(
@@ -43,20 +48,53 @@ export const useQuery = <RestType>(path: string, options?: Options) => {
         setError(error);
         setLoading(false);
       });
-
-    return () => {
-      abortController.abort();
-      unsubscribe(rerender);
-    };
   }, [path, JSON.stringify(options)]);
+
+  const fetchMore = useCallback(
+    (mergeFn: MergeFn<RestType>, options?: Pick<Options, "params">) => {
+      setLoadingMore(true);
+
+      const abortController = new AbortController();
+      abortControllers.add(abortController);
+      const signal = abortController.signal;
+
+      query<RestType>(
+        { path, signal, params: options?.params || undefined },
+        rerender
+      )
+        .then((newData) => {
+          setData(mergeFn(data as RestType, newData));
+          setLoadingMore(false);
+        })
+        .catch((error) => {
+          if (signal.aborted) {
+            // If the request has been cancelled,
+            // it’ll raise an error, that we don’t
+            // want to display.
+            return;
+          }
+
+          setError(error);
+          setLoadingMore(false);
+        });
+    },
+    [path, JSON.stringify(options)]
+  );
 
   useEffect(() => {
     if (options?.skip) {
       return;
     }
 
-    return refetch();
+    refetch();
+
+    return () => {
+      abortControllers.forEach((abortController) => {
+        abortController.abort();
+      });
+      unsubscribe(rerender);
+    };
   }, [refetch]);
 
-  return { data, error, loading, refetch };
+  return { data, error, loading, refetch, fetchMore, loadingMore };
 };
