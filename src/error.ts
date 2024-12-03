219e3a375a314e4dbae2673f744182e81877b53b
Basile Bruneau <basile@bruneau.email>
2024-12-03 18:51:17 +0100

    fix: don't throw if no content-type header in the response

--- src/error.ts
+++ src/error.ts
@@ -8,7 +8,7 @@ export class FetchError extends Error {
 
   async process() {
     this.status = this.response.status;
-    if (this.response.headers.get("content-type").includes("json")) {
+    if (this.response.headers.get("content-type")?.includes("json")) {
       this.data = await this.response.json();
     } else {
       this.data = await this.response.text();
